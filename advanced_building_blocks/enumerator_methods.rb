6ef0c57c4f5f10343e31688d24f312f4ffd05db5
Lawrence Archer <lawrence.archer@gmail.com>
2018-06-19 07:35:40 +0100

    finish enumarator_methods

--- advanced_building_blocks/enumerator_methods.rb
+++ advanced_building_blocks/enumerator_methods.rb
@@ -8,113 +8,128 @@ module Enumerable
 
 	def my_each_with_index
 		arr = self
-		arr.length.times do |index|
-			yield(arr[index], index)
+		arr.length.times do |x|
+			yield(arr[x], x)
 		end
+	end
+
+	def my_select
+		res = []
+		self.my_each do |x|
+			if yield (x)
+				res.push(x)
+			end
+		end 
+		res
+	end
+
+	def my_all?
+		res = []
+		self.length.times do |x|
+			res.push(yield(self[x]))
+		end
+		if res.include? false
+			false
+		else
+			true
+		end
+	end
+
+	def my_any?
+		self.length.times do |x|
+			a = yield(self[x])
+			if a==true
+				return true
+			end
+		end
+		false
+	end
+
+	def my_none?
+		self.length.times do |x|
+			a = yield(self[x])
+			if a==true
+				return false
+			end
+		end
+		true
+	end
+
+	def my_count(*arg)
+		puts "argument = #{arg}"
+		count = 0
+		if block_given? && arg.length != 0
+			return "You fucked up dickface - 0 or 1 arguments, or a block"
+		elsif block_given?
+			self.my_each do |x|
+				if yield(x)
+					count+=1 
+				end
+			end
+		elsif arg.length == 1
+			self.my_each do |x|
+				if x == arg
+					 count+=1
+				end
+			end
+		elsif arg.length == 0
+			count = self.length
+		else
+			return "You fucked up dickface - 0 or 1 arguments, or a block"
+		end
+
+		count
+	end
+
+	def my_map
+		puts block_given?
+		res = []
+		self.length.times do |x|
+			res.push(yield(self[x]))
+		end
+		res
+	end
+
+	def my_inject(*arg)
+		res = self[0]
+		if block_given?
+			if arg.length == 0
+				self.shift
+				self.length.times do |x|
+					res = yield(res, self[x])
+				end
+			elsif arg.length == 1
+				self.shift
+				self.push(arg[0])
+				self.length.times do |x|
+					res = yield(res, self[x])
+				end
+			else
+				return "wrong number of arguments, dickface"
+			end
+		else
+			if arg.length == 1
+				self.shift
+				self.length.times do |x|
+					res = eval ("#{res} #{arg[0]} #{self[x]}")
+				end
+			elsif arg.length == 2
+				self.shift
+				self.push(arg[0])
+				self.length.times do |x|
+					res = eval("#{res} #{arg[1]} #{self[x]}")
+				end
+			else
+				return "wrong number of arguments, dickface"
+			end
+		end
+		res
 	end
 end
 
-# module Enumerable
-# 	def my_each
-# 		copy = self
-# 		copy.length.times do |index|
-# 			yield self[index]
-# 		end
-# 	end
+longest = %w{ cat sheep bear }
+arr = [2,3,10,4,5,6]
 
-# 	def my_each_with_index
-# 		copy = self
-# 		copy.length.times do |index|
-# 			yield(copy[index], index)
-# 		end
-# 	end
-#
-# 	def my_select
-# 		copy = self
-# 		arr = []
-# 		for element in copy
-# 			if yield(element)
-# 				arr.push(element)
-# 			end
-# 		end
-# 		arr
-# 	end
-# 	def my_all?
-# 		copy = self
-# 		result = true
-# 		for element in copy
-# 			if !yield(element)
-# 				result = false
-# 			end
-# 		end
-# 		result
-# 	end
-
-# 	def my_any?
-# 		self.each do |item|
-# 			if yield
-# 				return true
-# 			end
-# 		end
-# 		false
-# 	end
-
-# 	def my_none?
-# 		if self.length == 0
-# 			true
-# 		else
-# 			false
-# 		end
-# 	end
-
-# 	def my_count
-# 		count = 0
-# 		self.my_each do |element|
-# 			if block_given?
-# 				count += 1 if yield(item)
-# 			else
-# 				count += 1
-# 			end
-# 		end
-# 		count
-# 	end
-
-# 	def my_map(proc=nil)
-# 		mapped = []
-# 		if proc != nil
-# 			self.my_each do |item|
-# 				mapped.push(proc.call(item))
-# 			end
-# 		else
-# 			self.my_each do |item|
-# 				mapped.push(yield(item))
-# 			end 	
-# 		end	
-# 	end
-
-# 	def my_inject(initial=0)
-# 		result = 0
-# 		if initial!= 0
-# 			result = initial
-# 		else
-# 			result = self[0]
-# 		end
-# 		self.my_each do |item|
-# 			yield(result, item)
-# 		end
-# 		result
-# 	end
-# end
-
-arr = [2,3,4]
-
-arr.my_each_with_index {|x, index| puts x.to_s + "  index: #{index}"}
-
-# arr.my_each_with_index do |x, index| 
-# 	puts x*2
-# 	puts "index = #{index}"
-# end
-
-# 3.times do |x|
-# 	puts x
-# end
+a = Proc.new{|x| x*2}
+b = arr.my_map(&a)
+print b
